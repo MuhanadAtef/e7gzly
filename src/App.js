22629c3e9fc3c3b7cfbc9b162799164be1365117
mmmacmp <mmmacmp@gmail.com>
2021-02-11 22:47:28 +0200

    Routing fix and adding protected route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React, { Component } from "react";
 import Home from "./components/Home";
-import { BrowserRouter, Route } from "react-router-dom";
+import MatchReservation from "./components/MatchReservation";
+import AdminDashboard from "./components/AdminDashboard";
+import { Router, Route, Switch } from "react-router-dom";
+import history from './components/history';
+import ProtectedRoute from "./components/ProtectedRoute";
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <Route exact path="/" component={Home}/>
-        </div>
-      </BrowserRouter>
+      <Router history={history}>
+        <Switch>
+          <div className="App">
+            <Route exact path="/" component={Home} />
+            <Route exact path="/match:match_id" component={MatchReservation} />
+            <ProtectedRoute exact path="/admin" component={AdminDashboard} />
+          </div>
+        </Switch>
+      </Router>
     );
+    // return (
+    //   <div className="App">
+    //     {/* <AdminDashboard /> */}
+    //     <MatchReservation />
+    //   </div>
+    // );
   }
 }
 
